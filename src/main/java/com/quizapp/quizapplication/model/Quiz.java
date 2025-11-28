543707e76b94929076a07a9465f4cc69124f5748
shrest21 <shrest21@gmail.com>
2025-11-29 01:35:32 +0530

    db error resolved after checking endpoints

--- src/main/java/com/quizapp/quizapplication/model/Quiz.java
+++ src/main/java/com/quizapp/quizapplication/model/Quiz.java
@@ -6,7 +6,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection = "Quiz")
+@Document(collection = "quiz")
 public class Quiz {
 
     @Id
